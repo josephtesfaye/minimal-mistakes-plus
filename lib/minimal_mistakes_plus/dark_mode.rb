ebdbb9db881430b741f98b251ad59051c2df2b62
Joseph Huang <josephtesfaye022@gmail.com>
2026-03-17 12:59:41 +0900

    Optimize dark mode
    Migrated `masthead.html` and `custom.html` files from `josephs-blog`, and
    removed the dark mode button injection logic. In this way it's easier to
    maintain, and cleaner to read than manipulating strings in a Ruby script.

--- lib/minimal_mistakes_plus/dark_mode.rb
+++ lib/minimal_mistakes_plus/dark_mode.rb
@@ -4,7 +4,13 @@ module MinimalMistakesPlus
     sass_dir = File.expand_path("../../_sass", __dir__)
     site.config['sass'] ||= {}
     site.config['sass']['load_paths'] ||= []
-    site.config['sass']['load_paths'] << sass_dir unless site.config['sass']['load_paths'].include?(sass_dir)
+    site.config['sass']['load_paths'] << sass_dir unless
+      site.config['sass']['load_paths'].include?(sass_dir)
+
+    # Inject the plugin's _includes directory to natively override the theme
+    includes_dir = File.expand_path("../../_includes", __dir__)
+    site.includes_load_paths.insert(1, includes_dir) unless
+      site.includes_load_paths.include?(includes_dir)
   end
 
   # 2. Register the JS file to be automatically copied to _site/assets/js/
@@ -12,29 +18,5 @@ module MinimalMistakesPlus
     next unless site.config['dark_mode_toggle']
     gem_dir = File.expand_path("../../", __dir__)
     site.static_files << Jekyll::StaticFile.new(site, gem_dir, 'assets/js', 'dark_mode_toggle.js')
-  end
-
-  # 3. High-performance Regex injection for the HTML UI
-  Jekyll::Hooks.register [:pages, :documents], :post_render do |doc|
-    next unless doc.site.config['dark_mode_toggle']
-    next unless doc.output.include?('greedy-nav') # Only process pages with a top nav
-
-    baseurl = doc.site.config['baseurl'] || ''
-    script_tag = %Q{<script src="#{baseurl}/assets/js/dark_mode_toggle.js"></script>}
-    button_tag = '<button class="theme-toggle-btn" onclick="switchTheme()"><i class="fas fa-moon"></i></button>'
-
-    # Inject Script right before </head>
-    unless doc.output.include?('<script src="' + baseurl + '/assets/js/dark_mode_toggle.js"')
-      doc.output = doc.output.sub('</head>', "  #{script_tag}\n</head>")
-    end
-
-    # Inject Button right after the Search Toggle or before the Greedy Nav Hamburger
-    unless doc.output.include?('<button class="theme-toggle-btn"')
-      if doc.output.include?('class="search__toggle"')
-        doc.output = doc.output.sub(/(<button[^>]*class="search__toggle"[^>]*>.*?<\/button>)/m, "\\1\n        #{button_tag}")
-      else
-        doc.output = doc.output.sub(/(<button[^>]*class="greedy-nav__toggle")/, "#{button_tag}\n        \\1")
-      end
-    end
   end
 end
